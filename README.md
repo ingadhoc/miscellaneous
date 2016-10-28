8573245a71c7e9461c10faaa93b0b4a58a03e0fc
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-10-28 15:55:08 -0300

    ADD base validator

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-[![Runbot Status](http://runbot.adhoc.com.ar/runbot/badge/flat/21/8.0.svg)](http://runbot.adhoc.com.ar/runbot/repo/github-com-ingadhoc-miscellaneous-21)
-[![Build Status](https://travis-ci.org/ingadhoc/miscellaneous.svg?branch=8.0)](https://travis-ci.org/ingadhoc/miscellaneous)
-[![Coverage Status](https://coveralls.io/repos/ingadhoc/miscellaneous/badge.png?branch=8.0)](https://coveralls.io/r/ingadhoc/miscellaneous?branch=8.0)
+[![Runbot Status](http://runbot.adhoc.com.ar/runbot/badge/flat/21/9.0.svg)](http://runbot.adhoc.com.ar/runbot/repo/github-com-ingadhoc-miscellaneous-21)
+[![Build Status](https://travis-ci.org/ingadhoc/miscellaneous.svg?branch=9.0)](https://travis-ci.org/ingadhoc/miscellaneous)
+[![Coverage Status](https://coveralls.io/repos/ingadhoc/miscellaneous/badge.png?branch=9.0)](https://coveralls.io/r/ingadhoc/miscellaneous?branch=9.0)
 [![Code Climate](https://codeclimate.com/github/ingadhoc/miscellaneous/badges/gpa.svg)](https://codeclimate.com/github/ingadhoc/miscellaneous)
 
 # ADHOC Miscellaneous Modules
@@ -11,7 +11,7 @@ This part will be replaced when running the oca-gen-addons-table script from OCA
 
 Translation Status
 ------------------
-[![Transifex Status](https://www.transifex.com/projects/p/ingadhoc-miscellaneous-8-0/chart/image_png)](https://www.transifex.com/projects/p/ingadhoc-miscellaneous-8-0)
+[![Transifex Status](https://www.transifex.com/projects/p/ingadhoc-miscellaneous-9-0/chart/image_png)](https://www.transifex.com/projects/p/ingadhoc-miscellaneous-9-0)
 
 ----
 
---
+++ base_validator/models/base_validator.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2014-2016 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models, api, _
+from openerp.exceptions import UserError, ValidationError
+from openerp.tools.safe_eval import safe_eval
+import re
+
+
+class BaseValidator(models.Model):
+    _name = "base.validator"
+
+    def _default_validation_code(self):
+        return _(
+            "\n# Python code. Use:\n"
+            "#  -  failed = True: specify that the value is not "
+            "valid.\n"
+            "#  -  value = 'something': overwrite value"
+            " value (for formatting for eg.).\n"
+            "# You can use the following:\n"
+            "#  - re: regex Python library\n"
+            "#  - self: browse_record of the current document type "
+            "browse_record\n"
+            "#  - value: string with the value to validate"
+        )
+
+    name = fields.Char(
+        required=True
+    )
+    validation_code = fields.Text(
+        'Validation code',
+        help="Python code called to validate and format a document number.",
+        required=True,
+        default=_default_validation_code
+    )
+    help_message = fields.Text(
+    )
+    input_test_string = fields.Char(
+    )
+    output_test_string = fields.Char(
+        compute='_compute_output_test_string',
+    )
+
+    @api.multi
+    def _validation_eval_context(self, value):
+        self.ensure_one()
+        return {'self': self,
+                'value': value,
+                're': re,
+                }
+
+    @api.multi
+    def validate_value(self, value, do_not_raise=False):
+        """Validate the given ID number
+        The method raises an openerp.exceptions.ValidationError if the eval of
+        python validation code fails
+        """
+        self.ensure_one()
+        eval_context = self._validation_eval_context(value)
+        msg = None
+        try:
+            safe_eval(self.validation_code,
+                      eval_context,
+                      mode='exec',
+                      nocopy=True)
+        except Exception as e:
+            msg = (_(
+                'Error when evaluating %s. Please check validation code.\n'
+                'Error:\n%s') % (self.name, e))
+        print 'eval_context', eval_context
+        if eval_context.get('failed', False):
+            msg = (_(
+                "'%s' is not a valid value for '%s'.\n%s") % (
+                value, self.name, self.help_message or ''))
+        elif eval_context.get('value', False):
+            value = eval_context.get('value', False)
+        if msg:
+            if do_not_raise:
+                return msg
+            else:
+                raise UserError(msg)
+        return value
+        # return {
+        #     'value': value, 'message': message, 'failed': failed,
+
+    @api.depends(
+        'validation_code',
+        'input_test_string',
+    )
+    def _compute_output_test_string(self):
+        for rec in self:
+            rec.output_test_string = rec.validate_value(
+                rec.input_test_string, True)
