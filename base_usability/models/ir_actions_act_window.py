87e937d6eb5502ea7ba80987f869f265299e518a
Katherine Zaoral <kz@adhoc.com.ar>
2018-03-26 16:19:43 -0300

    [ADD] base_usability: window action binding model
    This module design was completetly deprecated since ir.values model was
    removed from version 11.0. Also odoo now manage a field named binding_model_id
    which let us to do basically what we need.
    
    I updated the module to work with this field, only managing a button to
    simulate the same that does 'ADD CONTEXTUAL ACTION' in server action which is
    set binding_model_id (which is invisible) taking into account the source model
    field in the window action.
    
    A new improves were added that differ from version 9.0:
    
    - only create binding when the src_model is set.
    - if src_model is updated then the binding_model_id will be reset.
    - only show the add action button when the src_model is set.

--- base_usability/models/ir_actions_act_window.py
+++ base_usability/models/ir_actions_act_window.py
@@ -1,49 +1,26 @@
-
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo import models, api
 
 
 class IrActionsAct_window(models.Model):
     _inherit = 'ir.actions.act_window'
 
-    ir_values_id = fields.Many2one(
-        comodel_name='ir.values',
-        string='Ir Values',
-        compute='_compute_ir_values'
-    )
-
-    @api.multi
-    @api.depends('name', 'src_model')
-    def _compute_ir_values(self):
-        """ Solo si hay src_model puede haber accion ligada"""
-        for rec in self.filtered(lambda x: x.src_model):
-            rec.ir_values_id = rec.ir_values_id.search([
-                ('value', '=', "ir.actions.act_window,%s" % rec.id),
-                ('model', '=', rec.src_model)],
-                # ('name', '=', rec.name), ('model', '=', rec.src_model)],
-                limit=1)
-
     @api.multi
     def create_action(self):
         """ Create a contextual action for each act window. """
-        for rec in self:
-            ir_values = self.env['ir.values'].sudo().create({
-                'name': rec.name,
-                'model': rec.src_model,
-                'key2': 'client_action_multi',
-                'value': "ir.actions.act_window,%s" % rec.id,
-            })
-            rec.write({'ir_values_id': ir_values.id})
+        model = self.env['ir.model']
+        for rec in self.filtered('src_model'):
+            src_model = model.search([('model', '=', rec.src_model)])
+            rec.write({'binding_model_id': src_model.id,
+                       'binding_type': 'action'})
         return True
 
     @api.multi
     def unlink_action(self):
         """ Remove the contextual actions created for the act window. """
         self.check_access_rights('write', raise_exception=True)
-        for rec in self:
-            if rec.ir_values_id:
-                try:
-                    rec.ir_values_id.sudo().unlink()
-                except Exception:
-                    raise UserError(_('Deletion of the action record failed.'))
+        self.filtered('binding_model_id').write({'binding_model_id': False})
         return True
+
+    @api.onchange('src_model')
+    def update_binding_model_id(self):
+        self.binding_model_id = False
