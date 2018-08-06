2aa0e0d74d0a8245300ceb61b993e5033935ee2e
Katherine Zaoral <kz@adhoc.com.ar>
2018-08-06 11:37:13 -0300

    [ADD] mail_internal: New module to send internal messages (only employees)

---
+++ mail_internal/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Mail Internal',
+    'version': '11.0.1.0.0',
+    'category': 'Communications',
+    'sequence': 2,
+    'summary': 'Internal Messaging',
+    'author': 'ADHOC SA',
+    'website': 'http://www.adhoc.com/ar',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'views/assets.xml',
+        'data/mail_message_subtype_data.xml',
+    ],
+    'qweb': [
+        'static/src/xml/mail_internal.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
