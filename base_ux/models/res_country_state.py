f0a6308fb5bad449737a3920b5943487ffbcf9c3
Katherine Zaoral <kz@adhoc.com.ar>
2018-11-23 12:54:26 -0300

    [ADD] base_ux: order country states by name
    useful for website where we need to show country state order by name

--- base_ux/models/res_country_state.py
+++ base_ux/models/res_country_state.py
@@ -2,6 +2,11 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import ir_actions_act_window
-from . import res_partner
-from . import res_country_state
+from odoo import models
+
+
+class ResCountryState(models.Model):
+
+    _inherit = 'res.country.state'
+
+    _order = 'country_id,name'
