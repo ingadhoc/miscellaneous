a84bd62341a77dd0e73a7f7cce82de1690254beb
Lucas L. Soto <ls@adhoc.com.ar>
2020-01-30 13:52:17 -0300

    [MIG] Bump all repo's modules to 13.0.

--- mail_internal/__manifest__.py
+++ mail_internal/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mail Internal',
-    'version': '12.0.1.1.0',
+    'version': '13.0.1.0.0',
     'category': 'Communications',
     'sequence': 2,
     'summary': 'Internal Messaging',
