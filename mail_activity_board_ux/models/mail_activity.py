ab09944e95987aed841eb503546f8c29e7d50242
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-09-10 12:31:35 -0300

    [IMP] activity: use tree as default and other imp

--- mail_activity_board_ux/models/mail_activity.py
+++ mail_activity_board_ux/models/mail_activity.py
@@ -6,13 +6,11 @@ class MailActivity(models.Model):
     _inherit = "mail.activity"
 
     def open_activity_dashboard_form(self):
-        view_id = self.env.ref('mail_activity_board.mail_activity_view_form_board').id
         return {
             'type': 'ir.actions.act_window',
             'res_model': self._name,
             'view_type': 'form',
             'view_mode': 'form',
-            'views': [(view_id, 'form')],
             'target': 'current',
             'res_id': self.id,
             'context': dict(self._context),
