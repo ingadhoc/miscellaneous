13dc91f22162542685fc7a4681445d83746acf74
nmadhoc <nm@adhoc.com.ar>
2020-11-11 19:05:11 -0300

    [ADD] maintenance_ux

---
+++ maintenance_ux/models/maintenance_request.py
@@ -0,0 +1,21 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+
+from odoo import models
+
+
+class MaintenanceRequest(models.Model):
+    _name = 'maintenance.request'
+    _inherit = ['maintenance.request', 'rating.mixin']
+
+    def action_send_rating(self):
+        rating_template = self.env.ref('maintenance_ux.mail_template_maintenance_request_rating')
+        for order in self:
+            order.rating_send_request(rating_template, force_send=True)
+
+    def rating_get_partner_id(self):
+        if self.employee_id.user_id.partner_id:
+            return self.employee_id.user_id.partner_id
+        return self.env['res.partner']
