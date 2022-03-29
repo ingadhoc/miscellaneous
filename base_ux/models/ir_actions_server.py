b193baac8acf83918de97ce084695e6bbc581471
Augusto Weiss <awe@adhoc.com.ar>
2022-03-29 10:45:35 -0300

    [FIX] base_ux: fix bug migration importing re library

--- base_ux/models/ir_actions_server.py
+++ base_ux/models/ir_actions_server.py
@@ -1,18 +1,19 @@
 from odoo import models, api
 from odoo.tools import html2plaintext
-import re
-
+from odoo.tools.safe_eval import wrap_module
 
 class IrActionsServer(models.Model):
 
     _inherit = 'ir.actions.server'
+
+    re = wrap_module(__import__('re'), [])
 
     @api.model
     def _get_eval_context(self, action=None):
         """ Enable re python library to regex search and html2plaintext function from odoo tools """
         eval_context = super()._get_eval_context(action=action)
         eval_context.update({
-            're': re,
+            're': self.re,
             'html2plaintext': html2plaintext,
         })
         return eval_context
