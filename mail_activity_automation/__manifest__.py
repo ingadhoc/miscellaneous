ee3fc4518ed18623bb8a251ea555ffafa8944745
lav-adhoc <lav@adhoc.com.ar>
2023-08-04 18:45:40 +0000

    [ADD] mail_activity_automation: create module
    closes ingadhoc/miscellaneous#126
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

---
+++ mail_activity_automation/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': 'Mail Activity Automation',
+    'version': "16.0.1.0.0",
+    'category': 'Communications',
+    'sequence': 14,
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'views/mail_activity_type_views.xml',
+        'data/ir_cron_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ mail_activity_automation/models/mail_activity.py
@@ -0,0 +1,17 @@
+from odoo import models
+import datetime
+
+
+class MailActivity(models.Model):
+    _inherit = "mail.activity"
+
+    def _cron_run_activities(self):
+        activities = self.search([('activity_type_id.run_automatically', '=', True), ('date_deadline', '<=', datetime.date.today())])
+        for activity in activities:
+            activity.with_context(from_bot=True).action_done_schedule_next()
+
+    def _action_done(self, feedback=False, attachment_ids=None):
+        if self._context.get('from_bot') and self.activity_type_id.mail_template_ids:
+            for mail_template in self.activity_type_id.mail_template_ids:
+                self.env[self.res_model].browse(self.res_id).message_post_with_template(mail_template.id)
+        return super()._action_done(feedback=feedback, attachment_ids=attachment_ids)
