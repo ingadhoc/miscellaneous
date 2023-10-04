917fe474e06b38747c454c0c1bf749685c306986
Bruno Zanotti <bz@adhoc.com.ar>
2023-10-03 22:08:38 -0300

    [FIX] portal_backend: fix case internal user is follower

--- portal_backend/models/mail_thread.py
+++ portal_backend/models/mail_thread.py
@@ -8,3 +8,10 @@ class MailThread(models.AbstractModel):
         groups="base.group_user, portal_backend.group_portal_backend")
     message_follower_ids = fields.One2many(
         groups="base.group_user, portal_backend.group_portal_backend")
+
+    def _get_mail_thread_data(self, request_list):
+        """ This avoids to get an error when an internal user is follower of a record that a portal_backend has access
+        """
+        if 'followers' in request_list and self.env.user.has_group('portal_backend.group_portal_backend'):
+            request_list.remove('followers')
+        return super()._get_mail_thread_data(request_list)
