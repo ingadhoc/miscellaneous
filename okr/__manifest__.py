f227ae4a27b0a55eae8d4e605515dbd538ac8257
lav-adhoc <lav@adhoc.com.ar>
2023-11-02 11:04:23 -0300

    [IMP] Module okr by lav

---
+++ okr/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    'name': 'OKR',
+    'version': "16.0.1.0.0",
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/okr_views.xml',
+        'views/okr_menu.xml',
+    ],
+    'license': 'LGPL-3',
+}
