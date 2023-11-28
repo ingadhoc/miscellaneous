b210e891e9834d7f051ea36913789255a8d05801
Bruno Zanotti <bz@adhoc.com.ar>
2023-11-28 13:31:22 -0300

    [FIX] portal_backend: override load_menu as superuser to avoid personalization restrictions
    X-original-commit: a3c94c10ad32f61e446f5efa0e2e7948b7ee1918

--- portal_backend/models/ir_ui_menu.py
+++ portal_backend/models/ir_ui_menu.py
@@ -1,14 +1,17 @@
-from odoo import models, Command
-
+from odoo import models, Command, tools
 
 class IrUiMenu(models.Model):
     _inherit = 'ir.ui.menu'
 
+    @tools.ormcache_context('self._uid', 'debug', keys=('lang',))
     def load_menus(self, debug):
-        """ Assert all parent menus has internal group
+        """ Assert all parent menus has internal group.
         """
-        parent_menus_wo_group = self.search([('parent_id', '=', False), ('groups_id', '=', False)])
-        parent_menus_wo_group.write({
+        # NOTE:
+        # It is important to do it here to capture the case when portal_backend is already installed and the user
+        # installs another module with a parent menu without internal group.
+        parent_menus_wo_group = self.sudo().search([('parent_id', '=', False), ('groups_id', '=', False)])
+        parent_menus_wo_group.with_context('install_mode').write({
             'groups_id': [Command.link(self.env.ref('base.group_user').id)]
         })
         return super().load_menus(debug=debug)
