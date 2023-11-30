6a7ed18c8a3d7bb1558a549bc25559daacb43f88
Bruno Zanotti <bz@adhoc.com.ar>
2023-11-30 09:25:29 -0300

    [FIX] portal_backend: fix context key

--- portal_backend/models/ir_ui_menu.py
+++ portal_backend/models/ir_ui_menu.py
@@ -11,7 +11,7 @@ class IrUiMenu(models.Model):
         # It is important to do it here to capture the case when portal_backend is already installed and the user
         # installs another module with a parent menu without internal group.
         parent_menus_wo_group = self.sudo().search([('parent_id', '=', False), ('groups_id', '=', False)])
-        parent_menus_wo_group.with_context('install_mode').write({
+        parent_menus_wo_group.with_context(from_config=True).write({
             'groups_id': [Command.link(self.env.ref('base.group_user').id)]
         })
         return super().load_menus(debug=debug)
