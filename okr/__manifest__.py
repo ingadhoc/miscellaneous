8417de289d3a00e35001aac3eb2ead57f9d2c7e9
docker-odoo <lef@adhoc.com.ar>
2024-01-11 15:04:30 +0000

    [ADD] okr: new module for okr management

---
+++ okr/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'OKR',
+    'description': """
+        A module for Objectives and Key Results (OKR) management in Odoo.
+    """,
+    'version': "16.0.1.0.0",
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/okr_views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
