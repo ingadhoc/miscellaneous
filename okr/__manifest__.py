e870b8b65083a563d6e3e2f190cea969f6281460
docker-odoo <lef@adhoc.com.ar>
2024-02-02 16:00:52 +0000

    [MIG] okr: Migration to 17.0

--- okr/__manifest__.py
+++ okr/__manifest__.py
@@ -3,7 +3,7 @@
     'description': """
         A module for Objectives and Key Results (OKR) management in Odoo.
     """,
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
