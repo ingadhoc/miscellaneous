feccba887eaff78320ea018856c738bc8514121c
Rocío Vega <rov@adhoc.com.ar>
2024-02-06 08:37:39 -0300

    [ADD] okr_management: new module

---
+++ okr_management/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "OKR Managment",
+    "author": "Adhoc SA",
+    "version": "16.0.1.0",
+    "depends": [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/okr_view.xml',
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "application": True
+}
