b0cdaeeda000f545f32aaec5bc6d85588f7d424b
Juan José Scarafia <jjs@adhoc.com.ar>
2024-02-22 10:55:47 -0300

    [WIP]

--- okr/__manifest__.py
+++ okr/__manifest__.py
@@ -9,9 +9,12 @@
     ],
     'data': [
         'views/okr_objective_views.xml',
+        'views/okr_key_result_views.xml',
+        'security/security.xml',
         'security/ir.model.access.csv',
     ],
     'demo': [
+        'demo/okr_objective_demo.xml',
     ],
     'installable': True,
 }
--- okr/models/okr_key_result.py
+++ okr/models/okr_key_result.py
@@ -1,14 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
-class OkrObjective(models.Model):
+class OkrKeyResult(models.Model):
     _name = 'okr.key_result'
     _description = 'OKR Key Result'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(required=True)
     description = fields.Html()
     objective_id = fields.Many2one('okr.objective', required=True, ondelete='cascade')
-    user_id = fields.Many2one('res.users', string='Responsible')
+    department_ids = fields.Many2many('hr.department', string='Interdependencies')
+    user_id = fields.Many2one('res.users', string='Responsible', required=True)
     weight = fields.Integer(required=True)
     target = fields.Integer(required=True)
-    progress = fields.Integer()
+    result = fields.Integer()
+    progress = fields.Integer(compute='_compute_progress', store=True)
+    company_id = fields.Many2one(related='objective_id.company_id', store=True)
+
+    @api.depends('result', 'target')
+    def _compute_progress(self):
+        for rec in self:
+            rec.progress = rec.result / rec.target * 100 if rec.target else 100
