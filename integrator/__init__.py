ce89058e6b018e8d26819bb2e037332e4bed625e
augusto-weiss <awe@adhoc.com.ar>
2024-11-14 16:09:32 -0300

    [IMP] integrator: new module

---
+++ integrator/__init__.py
@@ -0,0 +1,4 @@
+# from .patch import *
+from . import models
+# from . import controllers
+# from . import wizards
---
+++ integrator/models/__init__.py
@@ -0,0 +1,4 @@
+from . import integrator_account
+from . import integrator_integration_script
+from . import integrator_integration
+from . import integrator_integration_script_line
