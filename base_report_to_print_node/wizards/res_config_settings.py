08067d1c1c58ddb89ed3c5f5410bfd2a27d1c5b3
Franco Leyes <lef@adhoc.com.ar>
2025-01-16 16:54:01 +0000

    [IMP] base_report_to_print_node: remove redundant string
    Part-of: ingadhoc/miscellaneous#237

--- base_report_to_print_node/wizards/res_config_settings.py
+++ base_report_to_print_node/wizards/res_config_settings.py
@@ -11,6 +11,5 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     print_node_api_key = fields.Char(
-        string="Print Node Api Key",
         config_parameter="base_report_to_print_node.api_key",
     )
