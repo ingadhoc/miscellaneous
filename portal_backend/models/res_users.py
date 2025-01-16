3ccc5ea8cdb5bba971e528270993c8d378c04ff2
Franco Leyes <lef@adhoc.com.ar>
2025-01-16 13:56:20 -0300

    [IMP] portal_backend: context overridden using dict

--- portal_backend/models/res_users.py
+++ portal_backend/models/res_users.py
@@ -83,10 +83,10 @@ class GroupsView(models.Model):
         # Write the arch on the original view
         xml_content = etree.tostring(arch)
         if xml_content != view.arch:
-            new_context = dict(view._context)
-            new_context.pop('install_filename', None)
-            new_context['lang'] = None
-            view.with_context(new_context).write({'arch': xml_content})
+            view.with_context(
+                install_filename=None, 
+                lang=None
+            ).write({'arch': xml_content})
 
     def _get_groups_between_separator(self, element):
         """ Get all the groups after a determined separator.
