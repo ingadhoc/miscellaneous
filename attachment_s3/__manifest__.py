39071c6a6a539a5f38a53d21798d2bbd019e4b14
Franco Leyes <lef@adhoc.com.ar>
2025-01-16 13:53:26 -0300

    [IMP] attachment_s3: add author ADHOC SA

--- attachment_s3/__manifest__.py
+++ attachment_s3/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Attachments on S3 storage",
     "summary": "Store assets and attachments on a S3 compatible object storage",
     "version": "17.0.1.0.0",
-    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "author": "Camptocamp, Odoo Community Association (OCA), ADHOC SA",
     "license": "AGPL-3",
     "category": "Knowledge Management",
     "depends": ["base", "base_attachment_object_storage"],
