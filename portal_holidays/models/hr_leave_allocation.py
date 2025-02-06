dc9feb19061b137a7bdd854720516aafd0567448
Celina Devigili <ced@adhoc.com.ar>
2025-02-06 20:22:57 +0000

    [IMP] portal_holidays: allow creating new leave allocation request
    closes ingadhoc/miscellaneous#240
    
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- portal_holidays/models/hr_leave_allocation.py
+++ portal_holidays/models/hr_leave_allocation.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import AccessError
 
 
 class HolidaysAllocation(models.Model):
@@ -9,3 +10,19 @@ class HolidaysAllocation(models.Model):
     employee_overtime = fields.Float(
         related="employee_id.total_overtime", groups="base.group_user,portal_holidays.group_portal_backend_holiday"
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """
+        Allows users in the portal holidays group to create allocation records
+        only for themselves. If the user passes the check, the method runs with
+        elevated privileges to bypass access restrictions if necessary.
+        """
+        if self.env.user.has_group("portal_holidays.group_portal_backend_holiday"):
+            user_employee_id = self.env.user.employee_id.id
+            for vals in vals_list:
+                if vals.get("employee_id") != user_employee_id:
+                    raise AccessError(_("You can only create allocations for yourself."))
+            self = self.sudo()
+
+        return super().create(vals_list)
