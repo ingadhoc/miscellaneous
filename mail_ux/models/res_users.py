a866430d3b789cf791fd996b594a891099509872
Martin Quinteros <maq@adhoc.com.ar>
2025-02-18 11:54:00 -0300

    [FIX] mail_ux: add send_message_delay to user SELF_READABLE_FIELDS and  SELF_WRITEABLE_FIELDS

--- mail_ux/models/res_users.py
+++ mail_ux/models/res_users.py
@@ -5,3 +5,11 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     send_message_delay = fields.Integer()
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ["send_message_delay"]
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + ["send_message_delay"]
