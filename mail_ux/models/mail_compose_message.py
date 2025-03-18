54f7cccef4c1704edcf0c6313939288aac72a5a6
Matias Peralta <matias_peralta1@hotmail.com>
2025-03-18 14:36:53 +0000

    [FIX]mail_ux: send email duplicate fixed
    closes ingadhoc/miscellaneous#249
    
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- mail_ux/models/mail_compose_message.py
+++ mail_ux/models/mail_compose_message.py
@@ -8,27 +8,14 @@ class MailComposeMessage(models.TransientModel):
 
     def _action_send_mail(self, auto_commit=False):
         """
-        Heredado para incluir un retraso de x segundos al enviar mensajes.
+        Heredado para incluir un retraso de x segundos al enviar mensajes si está configurado en el perfil.
         """
-        result_mails_su, result_messages = super(MailComposeMessage, self)._action_send_mail(auto_commit=auto_commit)
         if self.env.user.send_message_delay:
             scheduled_date = datetime.now() + timedelta(seconds=self.env.user.send_message_delay)
-            if self.composition_mode != "mass_mail":
-                for wizard in self:
-                    res_ids = wizard._evaluate_res_ids()
-                    for res_id in res_ids:
-                        self.env["mail.scheduled.message"].create(
-                            {
-                                "attachment_ids": [(6, 0, wizard.attachment_ids.ids)],
-                                "author_id": self.env.user.partner_id.id,
-                                "body": wizard.body,
-                                "model": wizard.model,
-                                "res_id": res_id,
-                                "partner_ids": [(6, 0, wizard.partner_ids.ids)],
-                                "scheduled_date": scheduled_date,
-                                "subject": wizard.subject,
-                                "notification_parameters": "{}",
-                            }
-                        )
-
+            super(MailComposeMessage, self).action_schedule_message(scheduled_date=scheduled_date)
+            result_mails_su, result_messages = None, None
+        else:
+            result_mails_su, result_messages = super(MailComposeMessage, self)._action_send_mail(
+                auto_commit=auto_commit
+            )
         return result_mails_su, result_messages
