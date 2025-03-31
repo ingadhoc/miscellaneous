206c471c09db21f9601913394bb2a1bbcf8d05c1
Matias Peralta <matias_peralta1@hotmail.com>
2025-03-31 17:12:21 -0300

    [FIX] mail_ux: error with send templated with automatic actions

--- mail_ux/models/mail_compose_message.py
+++ mail_ux/models/mail_compose_message.py
@@ -12,8 +12,8 @@ class MailComposeMessage(models.TransientModel):
         """
         if self.env.user.send_message_delay:
             scheduled_date = datetime.now() + timedelta(seconds=self.env.user.send_message_delay)
-            super(MailComposeMessage, self).action_schedule_message(scheduled_date=scheduled_date)
-            result_mails_su, result_messages = None, None
+            self.action_schedule_message(scheduled_date=scheduled_date)
+            result_mails_su, result_messages = self.env["mail.mail"].sudo(), self.env["mail.message"]
         else:
             result_mails_su, result_messages = super(MailComposeMessage, self)._action_send_mail(
                 auto_commit=auto_commit
