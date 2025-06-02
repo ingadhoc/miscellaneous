3a2356a8b93f76cc5da5de5a559b6b51b89c6260
augusto-weiss <awe@adhoc.com.ar>
2025-06-02 16:13:36 +0000

    [REM] base_ux: smtp disconnect exception added into odoo core
    closes ingadhoc/miscellaneous#264
    
    X-original-commit: 6a258c2dcedeb4c0b1c33e2b23d8fb927079ead4
    Signed-off-by: Alexis Lopez <loa@adhoc.com.ar>

--- base_ux/models/__init__.py
+++ base_ux/models/__init__.py
@@ -6,7 +6,6 @@ from . import ir_actions_act_window
 from . import res_partner
 from . import res_country_state
 from . import res_company
-from . import mail_mail
 from . import mail_template
 from . import ir_actions_server
 from . import mail_activity
