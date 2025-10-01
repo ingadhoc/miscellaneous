2ea70ea3058ecafc6ca807c24629626eee481edb
Virginia <vib@adhoc.inc>
2025-10-01 17:55:03 +0000

    [FIX] Update trans_export call to use environment instead of cursor

--- transifex_push/wizard/base_export_language.py
+++ transifex_push/wizard/base_export_language.py
@@ -33,7 +33,7 @@ class BaseLanguageExport(models.TransientModel):
 
         def _get_language_content(lang_code, module):
             with contextlib.closing(io.BytesIO()) as buf:
-                trans_export(lang_code, [module.name], buf, "po", self.env.cr)
+                trans_export(lang_code, [module.name], buf, "po", self.env)
                 content = buf.getvalue().decode("utf-8")
             return content
 
