998eea4731c9b2755e63d5a9ac350896571da45f
Franco Leyes <lef@adhoc.com.ar>
2025-10-03 17:56:04 +0000

    [MIG] portal_backend: Migration to 19.0
    closes ingadhoc/miscellaneous#282
    
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>

--- portal_backend/models/ir_ui_menu.py
+++ portal_backend/models/ir_ui_menu.py
@@ -1,17 +1,18 @@
-from odoo import Command, models, tools
+from odoo import Command, api, models, tools
 
 
 class IrUiMenu(models.Model):
     _inherit = "ir.ui.menu"
 
-    @tools.ormcache_context("self._uid", "debug", keys=("lang",))
+    @api.model
+    @tools.ormcache("self.env.uid", "debug", "self.env.lang")
     def load_menus(self, debug):
         """Assert all parent menus has internal group."""
         # NOTE:
         # It is important to do it here to capture the case when portal_backend is already installed and the user
         # installs another module with a parent menu without internal group.
-        parent_menus_wo_group = self.sudo().search([("parent_id", "=", False), ("groups_id", "=", False)])
+        parent_menus_wo_group = self.sudo().search([("parent_id", "=", False), ("group_ids", "=", False)])
         parent_menus_wo_group.with_context(from_config=True).write(
-            {"groups_id": [Command.link(self.env.ref("base.group_user").id)]}
+            {"group_ids": [Command.link(self.env.ref("base.group_user").id)]}
         )
         return super().load_menus(debug=debug)
