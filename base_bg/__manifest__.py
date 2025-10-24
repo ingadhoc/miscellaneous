ea5000b2d538ea24cb317cc009e90eb52176b9a3
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-24 12:44:04 +0000

    [MIG] base_bg: Migration to 19.0

--- base_bg/__manifest__.py
+++ base_bg/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Base Background Jobs",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Technical",
     "author": "ADHOC SA",
     "website": "https://www.adhoc.com.ar",
@@ -34,7 +34,7 @@
         "views/bg_job_views.xml",
         "data/ir_cron_data.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
