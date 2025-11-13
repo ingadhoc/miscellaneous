541cd2ae105ae2b21be20183a16cb397d931a427
Rocío Vega <rov@adhoc.com.ar>
2025-11-13 11:51:38 +0000

    [ADD] account_statement_import_sheet_file_bg: Add new module
    X-original-commit: 320707176795d586400ae2f7de017f3c20e2e8b9

---
+++ account_statement_import_sheet_file_bg/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Account statement import sheet file BG",
+    "version": "18.0.1.0.0",
+    "category": "Productivity/Documents",
+    "summary": "Integration between Documents and Base BG modules",
+    "depends": [
+        "base_bg",
+        "account_statement_import_sheet_file",
+    ],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+}
