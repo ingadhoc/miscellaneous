282374edb2f770af27621c551c47fb2a5710942d
Rocío Vega <rov@adhoc.com.ar>
2025-11-25 12:10:46 -0300

    [MIG] account_statement_import_sheet_file_bg: Migration to 19.0
    closes ingadhoc/miscellaneous#301
    
    Signed-off-by: Felipe Garcia Suez <feg@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_statement_import_sheet_file_bg/__manifest__.py
+++ account_statement_import_sheet_file_bg/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account statement import sheet file BG",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Productivity/Documents",
     "summary": "Integration between Documents and Base BG modules",
     "depends": [
