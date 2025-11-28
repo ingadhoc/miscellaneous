359d10ef599b8f3771ea482094e7eeaf513846ad
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-11-28 19:49:50 +0000

    [MIG] portal_timesheet_attendance: Migration to 19.0

--- portal_timesheet_attendance/__manifest__.py
+++ portal_timesheet_attendance/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Portal Timesheet Attendance",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Base",
     "sequence": 14,
     "summary": "",
@@ -35,6 +35,6 @@
         "security/ir.model.access.csv",
     ],
     "demo": [],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
 }
