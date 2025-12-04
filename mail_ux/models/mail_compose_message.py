dd0be4af674d1a9f3bae26d3b3e611670c99c7cd
augusto-weiss <awe@adhoc.com.ar>
2025-12-04 15:37:19 +0000

    [FIX] mail_ux: message with delay
    closes ingadhoc/miscellaneous#310
    
    Signed-off-by: Alexis Lopez <loa@adhoc.inc>

--- mail_ux/models/mail_compose_message.py
+++ mail_ux/models/mail_compose_message.py
@@ -6,16 +6,25 @@ from odoo import models
 class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
-    def _action_send_mail(self, auto_commit=False):
+    def _manage_mail_values(self, mail_values_all):
         """
         Heredado para incluir un retraso de x segundos al enviar mensajes si está configurado en el perfil.
         """
-        if self.env.user.send_message_delay:
-            scheduled_date = datetime.now() + timedelta(seconds=self.env.user.send_message_delay)
-            self.action_schedule_message(scheduled_date=scheduled_date)
-            result_mails_su, result_messages = self.env["mail.mail"].sudo(), self.env["mail.message"]
-        else:
-            result_mails_su, result_messages = super(MailComposeMessage, self)._action_send_mail(
-                auto_commit=auto_commit
-            )
-        return result_mails_su, result_messages
+        mail_values_all = super()._manage_mail_values(mail_values_all)
+        if not self.env.user.send_message_delay:
+            return mail_values_all
+
+        scheduled_date = datetime.now() + timedelta(seconds=self.env.user.send_message_delay)
+        for res_id, mail_values in mail_values_all.items():
+            mail_values["scheduled_date"] = scheduled_date
+        return mail_values_all
+
+    def _action_send_mail(self, auto_commit=False):
+        """
+        Cambio de logica: si hay un retraso configurado, se programa el envío en lugar de enviarlo inmediatamente.
+        """
+        if not self.env.user.send_message_delay:
+            return super()._action_send_mail(auto_commit=auto_commit)
+
+        self.action_schedule_message()
+        return self.env["mail.mail"].sudo(), self.env["mail.message"]
