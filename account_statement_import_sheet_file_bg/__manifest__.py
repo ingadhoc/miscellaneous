d5b8f289eff9cd518130512700edfa2fc4a93ab0
Rocío Vega <rov@adhoc.com.ar>
2025-12-17 10:54:21 -0300

    [IMP] _bg*: Add new improvements whent auto triger
    closes ingadhoc/miscellaneous#320
    
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- account_statement_import_sheet_file_bg/__manifest__.py
+++ account_statement_import_sheet_file_bg/__manifest__.py
@@ -1,13 +1,15 @@
 {
     "name": "Account statement import sheet file BG",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Productivity/Documents",
     "summary": "Integration between Documents and Base BG modules",
     "depends": [
         "base_bg",
         "account_statement_import_sheet_file",
     ],
-    "data": [],
+    "data": [
+        "data/ir_config_parameter_data.xml",
+    ],
     "demo": [],
     "installable": True,
     "auto_install": False,
