877583edb3f5be44d0400bdf1c2417a1425a1a8f
Virginia <vib@adhoc.inc>
2026-01-29 16:02:51 +0000

    [ADD] pot_github_push: backport

---
+++ pot_github_push/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "POT Generator",
+    "version": "18.0.1.0.0",
+    "category": "Tools",
+    "summary": "Helper module to generate POT files",
+    "author": "ADHOC SA",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
