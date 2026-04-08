c68b92976246c7928828613171d991094344b28c
“Juan <jr@adhoc.com.ar>
2026-04-08 17:44:42 +0000

    [IMP]mail_ux: Test email server connection from the UI
    X-original-commit: 969c9faf803f6f45d14958cf65fe24132ad45be6

--- mail_ux/models/__init__.py
+++ mail_ux/models/__init__.py
@@ -2,7 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from . import ir_http
+from . import ir_mail_server
 from . import mail_compose_message
 from . import mail_scheduled_message
 from . import res_users
-from . import ir_http
