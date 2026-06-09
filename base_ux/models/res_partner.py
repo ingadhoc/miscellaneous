ccbd62f0c42fdd5b8fbbdce431ae2ac15272af9f
les-adhoc <les@adhoc.inc>
2026-06-09 14:10:05 +0000

    [IMP] base_ux: add contact import template

--- base_ux/models/res_partner.py
+++ base_ux/models/res_partner.py
@@ -2,10 +2,21 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     active = fields.Boolean(tracking=True)
+
+    @api.model
+    def get_import_templates(self):
+        if self.env.context.get("contact_import"):
+            return [
+                {
+                    "label": _("Import Template for Contacts"),
+                    "template": "/base_ux/static/xls/res_partner.xlsx",
+                }
+            ]
+        return super().get_import_templates()
