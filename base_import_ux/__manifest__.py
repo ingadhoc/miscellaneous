5587ae0f78be456fe5afdbd63bf33dd423533a91
roboadhoc <roboadhoc@adhoc.com.ar>
2026-07-23 18:35:34 +0000

    [BOT] Bump version: base_import_ux 19.0.1.1.0
    Merged: ingadhoc/miscellaneous#426

--- base_import_ux/__manifest__.py
+++ base_import_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "First Steps",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "category": "Tools",
     "sequence": 14,
     "summary": "Centralizes the onboarding initial imports in a single panel "
